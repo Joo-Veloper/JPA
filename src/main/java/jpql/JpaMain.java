4536ae33f84c7e4f734a25c1b2cb4e24b7dc90d5
Joo-Veloper <psjoo3515@gmail.com>
2023-12-24 16:29:27 +0900

    서브 쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -25,30 +25,16 @@ public class JpaMain {
 
             member.setTeam(team);
 
-
             em.flush();
             em.clear();
-            //------------------------ Join ------------------------
-            // left join
-//            String query = "select m from Member m left join m.team t";
-//            List<Member> result = em.createQuery(query, Member.class)
-//                    .getResultList();
 
-            //setter join
-//            String query = "select m from Member m, Team t where m.username = t.name";
-//            List<Member> result = em.createQuery(query, Member.class)
-//                    .getResultList();
-            // 조인 대상 필터링
-//            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
-//            List<Member> result = em.createQuery(query, Member.class)
-//                    .getResultList();
-
-            // 연관관계 없는 엔티티 외부 조인
-            String query = "select m from Member m left join Team t on m.username = t.name";
+            // JPA 서브 쿼리
+            String query = "select (select avg(m1.age)from Member m1)as avgAge from Member m join Team t on m.username = t.name";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
             System.out.println("result =" + result.size());
+
 
             tx.commit();
         } catch (Exception e) {
@@ -61,6 +47,64 @@ public class JpaMain {
     }
 }
 
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+//public class JpaMain {
+//
+//    public static void main(String[] args) {
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin();
+//        try {
+//
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
+//
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(10);
+//
+//            member.setTeam(team);
+//
+//
+//            em.flush();
+//            em.clear();
+//            //------------------------ Join ------------------------
+//            // left join
+////            String query = "select m from Member m left join m.team t";
+////            List<Member> result = em.createQuery(query, Member.class)
+////                    .getResultList();
+//
+//            //setter join
+////            String query = "select m from Member m, Team t where m.username = t.name";
+////            List<Member> result = em.createQuery(query, Member.class)
+////                    .getResultList();
+//            // 조인 대상 필터링
+////            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+////            List<Member> result = em.createQuery(query, Member.class)
+////                    .getResultList();
+//
+//            // 연관관계 없는 엔티티 외부 조인
+//            String query = "select m from Member m left join Team t on m.username = t.name";
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .getResultList();
+//
+//            System.out.println("result =" + result.size());
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//            e.printStackTrace();
+//        } finally {
+//            em.close();
+//        }
+//        emf.close();
+//    }
+//}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
 
 //public class JpaMain {
 //
@@ -103,6 +147,7 @@ public class JpaMain {
 //    }
 //}
 
+//-------------------------------------------------------------------------------------------------------------------------------------------
 
 //public class JpaMain {
 //
