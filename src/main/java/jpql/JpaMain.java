44caba36a83c4491009cb54e6a33175c78e19de1
Joo-Veloper <psjoo3515@gmail.com>
2023-12-24 16:55:34 +0900

    JPQL 타입 표현

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
+import java.util.Objects;
 
 public class JpaMain {
 
@@ -20,21 +21,28 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("teamA");
             member.setAge(10);
+            member.setType(MemberType.ADMIN);
 
             member.setTeam(team);
 
             em.flush();
             em.clear();
 
-            // JPA 서브 쿼리
-            String query = "select (select avg(m1.age)from Member m1)as avgAge from Member m join Team t on m.username = t.name";
-            List<Member> result = em.createQuery(query, Member.class)
+            // JPQL 타입 표현
+            String query = "select m.username, 'HELLO', true From Member m " +
+                    "WHERE m.type = :userType";
+
+            List<Object[]> result = em.createQuery(query)
+                    .setParameter("userType", MemberType.ADMIN)
                     .getResultList();
 
-            System.out.println("result =" + result.size());
-
+            for(Object[] objects : result) {
+                System.out.println("objects = " + objects[0]);
+                System.out.println("objects = " + objects[1]);
+                System.out.println("objects = " + objects[2]);
+            }
 
             tx.commit();
         } catch (Exception e) {
@@ -46,6 +54,49 @@ public class JpaMain {
         emf.close();
     }
 }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+//public class JpaMain {
+//
+//    public static void main(String[] args) {
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin();
+//        try {
+//
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
+//
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(10);
+//
+//            member.setTeam(team);
+//
+//            em.flush();
+//            em.clear();
+//
+//            // JPA 서브 쿼리
+//            String query = "select (select avg(m1.age)from Member m1)as avgAge from Member m join Team t on m.username = t.name";
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .getResultList();
+//
+//            System.out.println("result =" + result.size());
+//
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//            e.printStackTrace();
+//        } finally {
+//            em.close();
+//        }
+//        emf.close();
+//    }
+//}
 
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
