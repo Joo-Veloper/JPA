dbbceef0147773681d89c465691db8c3185120c1
Seung Joo <psjoo3515@gmail.com>
2024-04-22 21:27:00 +0900

    Repository MethodQuery

--- src/main/java/jpa/realjpa/repository/MemberRepository.java
+++ src/main/java/jpa/realjpa/repository/MemberRepository.java
@@ -14,4 +14,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 //    @Query(name = "Member.findByUsername") 없어도 됨!
     List<Member> findByUsername(@Param("username") String username);
+
+    // 복잡한 JPQL 바로 넣어 사용 가능
+    // 동적 쿼리는 QueryDSL 사용
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
