47f386055b5e231771d83a6f9983d47fbda64445
Seung Joo <psjoo3515@gmail.com>
2024-04-22 20:25:37 +0900

    Create a query by method name

--- src/main/java/jpa/realjpa/repository/MemberRepository.java
+++ src/main/java/jpa/realjpa/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package jpa.realjpa.repository;
 import jpa.realjpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.List;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findTop3HelloBy();
 }
