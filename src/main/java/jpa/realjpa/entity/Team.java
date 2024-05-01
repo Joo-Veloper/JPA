5372989d943456fd202b442cdcb52715aa7aed54
Seung Joo <psjoo3515@gmail.com>
2024-05-01 21:50:15 +0900

    Auditing

--- src/main/java/jpa/realjpa/entity/Team.java
+++ src/main/java/jpa/realjpa/entity/Team.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Team {
+public class Team extends JpaBaseEntity{
     @Id @GeneratedValue
     @Column(name = "team_id")
     private Long id;
