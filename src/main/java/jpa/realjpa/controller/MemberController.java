e32d205e2fd772d8f58ef55f971f8f173f8c288f
Seung Joo <psjoo3515@gmail.com>
2024-05-02 21:44:54 +0900

    Paging & Sort

--- src/main/java/jpa/realjpa/controller/MemberController.java
+++ src/main/java/jpa/realjpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package jpa.realjpa.controller;
 
 import jakarta.annotation.PostConstruct;
+import jpa.realjpa.dto.MemberDto;
 import jpa.realjpa.entity.Member;
 import jpa.realjpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +28,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
